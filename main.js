41b7241da18c5a8d407e70d8904fadb31deb1d4f
Thomas Pham <thomas.pham96@yahoo.com>
2018-01-20 00:04:50 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+let win = null
+
+function createWindow (){
+    win = new BrowserWindow(
+        {
+            width: 800,
+            height: 800,
+            resizable: false,
+            frame: false
+        }
+    )
+    win.setMenu(null)
+
+    //loading index.html. 
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+      }))
+      
+      win.webContents.openDevTools()
+
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed',() =>{
+    app.quit()
+})
