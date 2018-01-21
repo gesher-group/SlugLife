d48183c6247788b868eef58882ab3e0c0edeb1d9
Thomas Pham <thomas.pham96@yahoo.com>
2018-01-20 19:12:16 -0800

    Parsed the nutirion information
    Using response-promise node module

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
-
+const request = require('request')
 const path = require('path')
 const url = require('url')
-
+const rp = require('request-promise');
 
 var urlName = "http://nutrition.sa.ucsc.edu/"
-var urlPrefix = "menuSamp.asp?locationNum="
-var urlSuffix = "&locationName=&sName=&naFlag="
+var urlPrefix = "pickMenu.asp?locationNum="
 
-//Example to call url to 9/10 
-//console.log(menuUrl['9/10'])
 var menuUrl = {}
-menuUrl['Cowell/Stevonson'] = urlName + urlPrefix + "05" + urlSuffix
-menuUrl['Crown/Merrill'] = urlName + urlPrefix + "20" + urlSuffix
-menuUrl['Porter/Kresge'] = urlName + urlPrefix + "25" + urlSuffix
-menuUrl['Carson/Oakes'] = urlName + urlPrefix + "30" + urlSuffix
-menuUrl['College 9/10'] = urlName + urlPrefix + "40" + urlSuffix
+menuUrl['Cowell/Stevonson'] = urlName + urlPrefix + "05"
+menuUrl['Crown/Merrill'] = urlName + urlPrefix + "20"
+menuUrl['Porter/Kresge'] = urlName + urlPrefix + "25"
+menuUrl['Carson/Oakes'] = urlName + urlPrefix + "30"
+menuUrl['College 9/10'] = urlName + urlPrefix + "40"
 
+//type is Breakfast, Lunch, Dinner.
+function getMealOptions(type,collegeName){
+    return menuUrl[collegeName] + "&mealName=" + type
+}
+
+function getMenuNutrition(menu){
+    return rp(menu).then(body =>{
+        let regExp = /a href='[a-zA-Z0-9.?=&%']+/g
+        let links = body.match(regExp)
+        
+        let stringLinks = []
+        for(let i = 0; i < links.length; i++){
+            stringLinks.push(links[i].toString())
+        }
+        for(let i = 0; i < stringLinks.length; i++){
+            stringLinks[i] = stringLinks[i].slice(8, stringLinks[i].length-2)
+            stringLinks[i] = urlName + stringLinks[i]
+        }
+        return stringLinks
+    })
+}
+//Example function call.
+// getMenuNutrition(getMealOptions("Breakfast", 'College 9/10')).then(links =>{
+//     console.log(links)
+//  })
 
 
 let win = null
@@ -24,8 +46,8 @@ var diningMenus = null
 
 function openDiningHallMenu (id){
     let win = new BrowserWindow({
-        width: 200,
-        height: 200,
+        width: 500,
+        height: 500,
         title: id
     })
     win.show()
@@ -35,7 +57,7 @@ app.on('ready', function(){
     win = new BrowserWindow(
         {
             width: 1000,
-            height: 1000,
+            height: 800,
             resizable: false,
             frame: true
         }
@@ -49,13 +71,19 @@ app.on('ready', function(){
         slashes: true
       }))
       
-      win.webContents.openDevTools()
+    win.webContents.openDevTools()
 
-      ipcMain.on('open-menu', function(event, id){
+    ipcMain.on('open-menu', function(event, id){
         openDiningHallMenu(id)
-      })
+    })
 })
 
 app.on('window-all-closed',() =>{
     app.quit()
 })
+
+
+module.exports = {
+    getMealOptions: getMealOptions,
+    getMenuNutrition: getMenuNutrition
+}
