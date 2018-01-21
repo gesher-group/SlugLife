d709ddeb815f78bc48adb0b27d64d0d642221f7e
Alysia <atran46@ucsc.edu>
2018-01-21 01:30:17 -0800

    Changed IDs in index.js and index.html

--- js/index.js
+++ js/index.js
@@ -4,6 +4,16 @@ function openMenu(element){
   ipcRenderer.send('open-menu', element.id)
 }
 
+var color = "RED"
+
+function highlight(container,what,spanClass) {
+    var content = container.innerHTML,
+        pattern = new RegExp('(>[^<.]*)(' + what + ')([^<.]*)','g'),
+        replaceWith = '$1<span ' + ( spanClass ? 'class="' + spanClass + '"' : '' ) + '">$2</span>$3',
+        highlighted = content.replace(pattern,replaceWith);
+    return (container.innerHTML = highlighted) !== content;
+}
+
 $('.open-overlay').click(function() {
   var overlay_navigation = $('.overlay-navigation'),
     nav_item_1 = $('nav li:nth-of-type(1)'),
@@ -40,3 +50,247 @@ $('.open-overlay').click(function() {
   }
 })
 
+var currentDate = new Date()
+    var day = currentDate.getDay()
+    var hour = currentDate.getHours()
+    var min = currentDate.getMinutes();
+    var dates = currentDate.toDateString()
+    var time = currentDate.toLocaleTimeString()
+
+    console.log(dates + ' '+ time)
+    console.log("current day =" + day)
+    console.log("hour=" + hour)
+    console.log("min=" + min)
+
+   // day = 0
+   // hour = 12 
+   // min = 31
+    console.log("day=" + day + " test time=" + hour + ":" + min)
+
+    //College9/10
+    if (day == 0 || day == 6) {     
+        if (7 <= hour && hour < 10) 
+            document.getElementById("c9breakfast").style.background= color
+        else if (10 <= hour && hour < 14)
+            document.getElementById("c9brunch").style.background= color
+        else if (14 <= hour && hour < 17)
+            document.getElementById("c9cont").style.background= color
+        else if (17 <= hour && hour < 20) 
+            document.getElementById("c9dinner").style.background= color
+        else if (20 <= hour && hour < 23)
+            document.getElementById("c9late").style.background= color
+        else 
+            document.getElementById("c9closed").style.background= color
+    }
+    else if (1 <= day && day < 6) {
+        if (hour == 6 && min >= 30) {
+            document.getElementById("c9breakfast").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("c9breakfast").style.background= color
+            }
+            else {
+                document.getElementById("c9brunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("c9brunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17) {
+            document.getElementById("c9cont").style.background= color
+        }
+        else if (17 <= hour && hour < 20) {
+            document.getElementById("c9dinner").style.background= color
+        }
+        else if (20 <= hour && hour < 23)
+            document.getElementById("c9late").style.background= color
+        else 
+            document.getElementById("c9closed").style.background= color
+    }
+
+    //Cowell/Stevenson
+    if (day == 0) {     
+        if (7 <= hour && hour < 10) 
+            document.getElementById("csbreak").style.background= color
+        else if (10 <= hour && hour < 14)
+            document.getElementById("csbrunch").style.background= color
+        else if (14 <= hour && hour < 17)
+            document.getElementById("cscont").style.background= color
+        else if (17 <= hour && hour < 20) 
+            document.getElementById("csdinner").style.background= color
+        else if (20 <= hour && hour < 23)
+            document.getElementById("cslate").style.background= color
+        else 
+            document.getElementById("csclosed").style.background= color
+    }
+    else if (day == 6) {        
+        if (hour == 6 && min >= 30) {
+            document.getElementById("csbreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("csbreak").style.background= color
+            }
+            else {
+                document.getElementById("csbrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("csbrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17)
+            document.getElementById("cscont").style.background= color
+        else if (17 <= hour && hour < 20) 
+            document.getElementById("csdinner").style.background= color
+        else 
+            document.getElementById("csclosed").style.background= color
+    }
+    else if (1 <= day && day < 6) {
+        if (hour == 6 && min >= 30) {
+            document.getElementById("csbreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("csbreak").style.background= color
+            }
+            else {
+                document.getElementById("csbrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("csbrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17) {
+            document.getElementById("cscont").style.background= color
+        }
+        else if (17 <= hour && hour < 20) {
+            document.getElementById("csdinner").style.background= color
+        }
+        else if (20 <= hour && hour < 23)
+            document.getElementById("cslate").style.background= color
+        else 
+            document.getElementById("csclosed").style.background= color
+    }
+
+    //Carson/Oakes
+    if (day == 0) {     
+        if (7 <= hour && hour < 10) 
+            document.getElementById("cobreak").style.background= color
+        else if (10 <= hour && hour < 14)
+            document.getElementById("cobrunch").style.background= color
+        else if (14 <= hour && hour < 17)
+            document.getElementById("cocont").style.background= color
+        else if (17 <= hour && hour < 20) 
+            document.getElementById("codinner").style.background= color
+        else if (20 <= hour && hour < 23)
+            document.getElementById("colate").style.background= color
+        else 
+            document.getElementById("coclosed").style.background= color
+    }
+    else if (day == 6) {        
+        if (hour == 6 && min >= 30) {
+            document.getElementById("cobreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("cobreak").style.background= color
+            }
+            else {
+                document.getElementById("cobrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("cobrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17)
+            document.getElementById("cocont").style.background= color
+        else if (17 <= hour && hour < 20) 
+            document.getElementById("codinner").style.background= color
+        else 
+            document.getElementById("coclosed").style.background= color
+    }
+    else if (1 <= day || day < 6) {
+        if (hour == 6 && min >= 30) {
+            document.getElementById("cobreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("cobreak").style.background= color
+            }
+            else {
+                document.getElementById("cobrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("cobrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17) {
+            document.getElementById("cocont").style.background= color
+        }
+        else if (17 <= hour && hour < 20) {
+            document.getElementById("codinner").style.background= color
+        }
+        else if (20 <= hour && hour < 23)
+            document.getElementById("colate").style.background= color
+        else 
+            document.getElementById("coclosed").style.background= color
+    }
+
+    //Crown/Merill
+    if (day == 0 || day == 6) {
+        document.getElementById("cmclosed").style.background= color
+    }
+    else if (1 <= day && day < 6) {
+        if (hour == 6 && min >= 30) {
+            document.getElementById("cmbreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("cmbreak").style.background= color
+            }
+            else {
+                document.getElementById("cmbrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("cmbrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17) {
+            document.getElementById("cmcont").style.background= color
+        }
+        else if (17 <= hour && hour < 20) {
+            document.getElementById("cmdinner").style.background= color
+        }
+        else 
+            document.getElementById("cmclosed").style.background= color
+    }
+
+    //Porter/Kresge
+    if (day == 0 || day == 6) {
+        document.getElementById("pkclosed").style.background= color
+    }
+    else if (1 <= day && day < 6) {
+        if (hour == 6 && min >= 30) {
+            document.getElementById("pkbreak").style.background= color
+        }
+        else if (hour >= 7 && hour < 12) {
+            if (hour == 11 && min < 30) {
+                document.getElementById("pkbreak").style.background= color
+            }
+            else {
+                document.getElementById("pkbrunch").style.background= color
+            }
+        }
+        else if (hour >= 12 && hour < 14 ) {
+            document.getElementById("pkbrunch").style.background= color
+        }
+        else if (14 <= hour && hour < 17) {
+            document.getElementById("pkcont").style.background= color
+        }
+        else if (17 <= hour && hour < 19) {
+            document.getElementById("pkdinner").style.background= color
+        }
+        else 
+            document.getElementById("pkclosed").style.background= color
+    }
